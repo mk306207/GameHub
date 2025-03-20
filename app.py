813588a3886cbf43f72ae0694189a972db57cfb6
mk306207 <114991401+mk306207@users.noreply.github.com>
2025-03-20 16:09:04 +0100

    Change of technology

--- app.py
+++
@@ -1,7 +0,0 @@
-from views import *
-from flask import Flask
-app = Flask(__name__)
-app.register_blueprint(views, url_prefix = "/")
-
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
