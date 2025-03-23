5ac02a15c3241629da854e2236ad1f73eba19c64
mk306207 <kolbermateusz@gmail.com>
2025-03-23 23:50:47 +0100

    logging base

--- GameHub/myapp/views.py
+++ GameHub/myapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def login(request):
+    return render(request, 'login.html')
