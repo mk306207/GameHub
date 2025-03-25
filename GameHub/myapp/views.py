f6dd352ebfde6edb5b90a7cdf5b657b3543d3a1e
mk306207 <kolbermateusz@gmail.com>
2025-03-25 18:07:53 +0100

    changes

--- GameHub/myapp/views.py
+++ GameHub/myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'home.html')
 
 def login(request):
     return render(request, 'login.html')
