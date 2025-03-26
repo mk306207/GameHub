fdfcd0de0418fdfe011f746bdb313f040ad15988
mk306207 <kolbermateusz@gmail.com>
2025-03-26 13:23:35 +0100

    logging in improvements

--- GameHub/myapp/models.py
+++ GameHub/myapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class usersList(models.Model):
+    login = models.CharField(max_length=25)
+    password = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.login} - {self.password}"
+
+class userInfo(models.Model):
+    logANDpsw = models.ForeignKey(usersList,on_delete=models.CASCADE)
+    email = models.EmailField()
+    
+    def __str__(self):
+        return self.email
--- GameHub/myapp/views.py
+++ GameHub/myapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse,redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from myapp.models import usersList
 
 # Create your views here.
 def home(request):
@@ -6,3 +9,28 @@ def home(request):
 
 def login(request):
     return render(request, 'login.html')
+
+def login_manager(request):
+    if request.method == "POST":
+        login_input = request.POST.get("login")
+        password_input = request.POST.get("password")
+        print(f"Login: {login_input}, Password: {password_input}")
+        try:
+            user = usersList.objects.get(login=login_input, password=password_input)
+            request.session["user_id"] = user.id
+            messages.success(request, "Succesfully logged in!")
+            return redirect("dashboard")
+        except usersList.DoesNotExist:
+            messages.error(request, "Login or password incorrect!")
+
+    return render(request, "login.html")
+
+def logout(request):
+    request.session.flush()
+    return redirect("login")
+
+def dashboard(request):
+    if "user_id" not in request.session:
+        return redirect("login")
+    
+    return render(request, "dashboard.html")
